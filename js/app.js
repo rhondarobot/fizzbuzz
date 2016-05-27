3ff24396bb5d4f90e0071d03f9c85d79254a77ad
rhondarobot <rrachellej@gmail.com>
2016-05-27 12:21:33 -0700

    form not working. had js scripts entered wrong in html. looking for more errors.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,15 @@
 $(document).ready(function() {
-	$('form').on('submit', function(event) {
+	$('#formInput').on('submit', function(event) {
     event.preventDefault(); 
-    alert('enter a number 1-100 first!')
-
-    console.log('#submit')
-
-
-	var f = 3;
-	var b = 5;
-
-	var enteredNum = $(#inputNumber).val();
-		if (enteredNum/3) {
-			$('ul').append('<li>' + 'fizz' + '</li>');
-		} else if (enteredNum/5) {
-			$('ul').append('<li>' + 'buzz' + '</li>');
-		} else {
-			$('ul').append('<li>' + 'enteredNum' + '</li>');
-		}	
-
+ 
+ 	var enteredNum = $('#inputNumber').val();
+ 	if(enteredNum === "") {
+ 		alert("enter a number!");
+    }
+     else {
+     	$('ul').append('<li>' + enteredNum + '</li>');
+     	$('#inputNumber').val('');
+     }
 });
 
 

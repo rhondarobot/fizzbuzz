4766fd4502b6df0962d5c38bcbc1480cff414d1e
rhondarobot <rrachellej@gmail.com>
2016-05-30 13:05:31 -0700

    fizzbuzz complete

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
   $('#inputForm').on('submit', function(event) {
-    event.preventDefault();  
-   
-     var newItem = $('#inputNumber').val();  
-     if (newItem === "") {
-        alert("Please enter an item first!")
-  }  else {
+    event.preventDefault(); 
+    
+    var newItem = $('#inputNumber').val();
+ if (newItem === "") {
+        alert("Please enter an item first!") 
+  } else if (newItem % 3 === 0 && newItem % 5 === 0) {
+     $('ul').append('<li>' + 'fizzbuzz' + '</li>');    
+  } else if (newItem % 3 === 0) {
+    $('ul').append('<li>' + 'fizz' + '</li>');
+  } else if (newItem % 5 === 0) {
+    $('ul').append('<li>' + 'buzz' + '</li>');
+  } 
+    else {
         $('ul').append('<li>' + newItem + '</li>');   
         $('#inputNumber').val('');
   }
   })
+  
 }); 
   //will not add a blank item when alert is shown and will only add <li>s upon entering text
-
-
-	

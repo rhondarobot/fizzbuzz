3555105039016b00ad7c1b41802cdd2c08f51a23
rhondarobot <rrachellej@gmail.com>
2016-05-30 12:01:23 -0700

    form working--now to add variables

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
-	$('#formInput').on('submit', function(event) {
-    event.preventDefault(); 
- 
- 	var enteredNum = $('#inputNumber').val();
- 	if(enteredNum === "") {
- 		alert("enter a number!");
-    }
-     else {
-     	$('ul').append('<li>' + enteredNum + '</li>');
-     	$('#inputNumber').val('');
-     }
-});
-
+  $('#inputForm').on('submit', function(event) {
+    event.preventDefault();  
+   
+     var newItem = $('#inputNumber').val();  
+     if (newItem === "") {
+        alert("Please enter an item first!")
+  }  else {
+        $('ul').append('<li>' + newItem + '</li>');   
+        $('#inputNumber').val('');
+  }
+  })
+}); 
+  //will not add a blank item when alert is shown and will only add <li>s upon entering text
 
 
 	
-
